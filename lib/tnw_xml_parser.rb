36ac6b4cd80e9503266c9f3cea6335e97d5cd264
Ted Yang <ted.yang@clio.com>
2020-08-13 16:09:50 -0700

    add rspec for this gem

--- lib/tnw_xml_parser.rb
+++ lib/tnw_xml_parser.rb
@@ -1,5 +1,22 @@
+require "uri"
+
 class TnwXmlParser
-  def hello
-    puts "hello world!"
+
+  def initialize(xml_url:)
+    validate_xml_url(xml_url)
+
+    @xml_url = xml_url
+  end
+
+  def parse
+    # to be implemented
+  end
+
+  private
+
+  def validate_xml_url(url_string)
+    uri = URI.parse(url_string)
+    raise ArgumentError.new("Url must be from www.sec.gov") if uri.host != "www.sec.gov"
+    raise ArgumentError.new("Url must ends with .xml") if !uri.path.end_with?(".xml")
   end
 end
--- tnw_xml_parser.gemspec
+++ tnw_xml_parser.gemspec
@@ -8,4 +8,7 @@ Gem::Specification.new do |s|
   s.files       = ["lib/tnw_xml_parser.rb"]
   s.homepage    = "https://github.com/tedyangx/tnw_xml_parser"
   s.license     = "MIT"
+
+  s.add_development_dependency "rspec"
+  s.add_development_dependency "pry"
 end
