b11216863a08d7d0db29a2a52c9d49021c286dcc
Ted Yang <ted.yang@clio.com>
2020-08-14 17:47:54 -0700

    add the #read_data and #convert_to_hash methods

--- lib/tnw_xml_parser.rb
+++ lib/tnw_xml_parser.rb
@@ -4,21 +4,49 @@ require "open-uri"
 
 class TnwXmlParser
 
-  attr_reader :xml_url, :xml_doc
-
-  def self.parse(xml_url)
-    new(xml_url)
-  end
+  attr_reader :xml_url, :xml_doc, :prefix
 
   def initialize(xml_url)
     validate_xml_url(xml_url)
 
     @xml_url = xml_url
-    @xml_doc = Nokogiri::HTML(open(@xml_url))
+  end
+
+  def read_data
+    @xml_doc = Nokogiri::XML(open(@xml_url)) do |config|
+      config.strict.noblanks
+    end
+
+    if @xml_doc.css("infoTable").size > 0
+      @prefix = ""
+    elsif @xml_doc.css("n1:infoTable").size > 0
+      @prefix = "n1:"
+    else
+      raise "infoTable missing from the XML"
+    end
+    
+    @xml_doc
   end
 
   def convert_to_hash
-    # to be implemented
+    raise "Please read_data first before converting to json" if xml_doc.nil?
+    result = {}
+    xml_doc.css("#{prefix}infoTable").each do |info_table|
+      cusip = info_table.children.find { |ele| ele.name == "cusip" }.content.upcase
+      if result[cusip].nil?
+        result[cusip] = {}
+        result[cusip]["cusip"] = cusip
+        result[cusip]["nameOfIssuer"] = info_table.children.find { |ele| ele.name == "nameOfIssuer" }.content
+        result[cusip]["value"] = info_table.children.find { |ele| ele.name == "value" }.content.to_i
+        result[cusip]["shrsOrPrnAmt"] = info_table.children.find { |ele| ele.name == "shrsOrPrnAmt" }.children[0].content.to_i
+        result[cusip]["shrsOrPrnAmt type"] = info_table.children.find { |ele| ele.name == "shrsOrPrnAmt" }.children[1].content
+        result[cusip]["putCall"] = info_table.children.find { |ele| ele.name == "putCall" }&.content
+      else
+        result[cusip]["value"] += info_table.children.find { |ele| ele.name == "value" }.content.to_i
+        result[cusip]["shrsOrPrnAmt"] += info_table.children.find { |ele| ele.name == "shrsOrPrnAmt" }.children[0].content.to_i
+      end
+    end
+    result
   end
 
   private
--- spec/tnw_xml_parser_spec.rb
+++ spec/tnw_xml_parser_spec.rb
@@ -1,7 +1,38 @@
 RSpec.describe TnwXmlParser do
-  it "does something" do
+  it "raises ArgumentError if host is not sec.gov" do
+    expect do
+      parser = TnwXmlParser.new("https://google.com/asdf.xml")
+    end.to raise_error(ArgumentError)
+  end
+
+  it "raises ArgumentError if not xml link" do
+    expect do
+      parser = TnwXmlParser.new("https://google.com/asdf.xml")
+    end.to raise_error(ArgumentError)
+  end
+
+  it "does not raise ArgumentError" do
+    expect do
+      parser = TnwXmlParser.new("https://www.sec.gov/Archives/edgar/data/1067983/000095012320005345/form13fInfoTable.xml")
+    end.to_not raise_error
+  end
+
+  it "can read the xml data as a Nokogiri::XML::Document" do
     parser = TnwXmlParser.new("https://www.sec.gov/Archives/edgar/data/1067983/000095012320005345/form13fInfoTable.xml")
-    binding.pry
-    parser
+    parser.read_data
+    expect(parser.xml_doc).to be_an_instance_of(Nokogiri::XML::Document)
+  end
+
+  it "raises error if the XML has no infoTable" do
+    parser = TnwXmlParser.new("https://www.sec.gov/Archives/edgar/data/813917/000081391720000038/primary_doc.xml")
+    expect do
+      parser.read_data
+    end.to raise_error(/infoTable missing/)
+  end
+
+  it "converts to hash" do
+    parser = TnwXmlParser.new("https://www.sec.gov/Archives/edgar/data/1067983/000095012320005345/form13fInfoTable.xml")
+    parser.read_data
+    parser.convert_to_hash
   end
 end
